90389d04625872e972990000ae8a13d3372f8070
Joita Fabian Gabriel <joitafabi@gmail.com>
2025-01-03 17:12:46 +0200

    Adaugare matrice de testare

--- CoffeManagementSystem/src/optiunea6.cpp
+++ CoffeManagementSystem/src/optiunea6.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+// o sa adaugam datele in fisiere dupa ce verificam data angajatul produsul sau ingredietul nu mai exista
 void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
 {
     CSV newCSV;
@@ -32,14 +33,13 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
 
     vector<string> headers;
     vector<vector<string>> data;
-    string dataString;
     string finalpath;
 
     string numeAngajat, functie, locatie;
     int salariu = 0, varsta = 0;
     string oraInceput, oraSfarsit;
-    string numeProdus, numeClient, produseStr;
-    float pretProdus = 0.0, pretTotal = 0.0;
+    string numeProdus;
+    float pretProdus = 0.0;
     string ingredientName;
     int ingredientGramaj = 0;
 
@@ -48,7 +48,9 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
     case 1:
     {
         finalpath = basePath + "/" + cafeLocation + "/" + ((languageOption == "EN") ? "Employees_Roles.csv" : "Angajati_Functii.csv");
-        if (newCSV.readCSV(finalpath).empty())
+        auto csvData = newCSV.readCSV(finalpath);
+
+        if (csvData.empty())
         {
             headers = {(languageOption == "EN") ? "Employee Name" : "Nume Angajat",
                        (languageOption == "EN") ? "Age" : "Varsta",
@@ -59,10 +61,26 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
                        (languageOption == "EN") ? "End Time" : "Ora Sfarsit"};
         }
 
-        cout << ((languageOption == "EN") ? "Enter employee details in the following order:" : "Se citesc datele in urmatoarea ordine:") << endl;
         cout << ((languageOption == "EN") ? "1. Employee Name: " : "1. Nume Angajat: ");
         cin.ignore();
         std::getline(cin, numeAngajat);
+
+        bool alreadyExists = false;
+        for (const auto &row : csvData)
+        {
+            if (!row.empty() && row[0] == numeAngajat)
+            {
+                alreadyExists = true;
+                break;
+            }
+        }
+
+        if (alreadyExists)
+        {
+            cout << ((languageOption == "EN") ? "Employee already exists with this name: " : "Angajatul cu acest nume exista deja: ") << numeAngajat << endl;
+            break;
+        }
+
         cout << ((languageOption == "EN") ? "2. Age: " : "2. Varsta: ");
         cin >> varsta;
         cout << ((languageOption == "EN") ? "3. Salary: " : "3. Salariu: ");
@@ -75,7 +93,6 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
         cin >> oraSfarsit;
 
         locatie = cafeLocation;
-
         // Adaugam angajatul la date
         data.push_back({numeAngajat, to_string(varsta), to_string(salariu), functie, locatie, oraInceput, oraSfarsit});
         break;
@@ -83,7 +100,9 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
     case 2:
     {
         finalpath = basePath + "/" + cafeLocation + "/" + ((languageOption == "EN") ? "Products_Prices.csv" : "Produse_Preturi.csv");
-        if (newCSV.readCSV(finalpath).empty())
+        auto csvData = newCSV.readCSV(finalpath);
+
+        if (csvData.empty())
         {
             headers = {(languageOption == "EN") ? "Product" : "Produs", (languageOption == "EN") ? "Price" : "Pret"};
         }
@@ -91,9 +110,25 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
         cout << ((languageOption == "EN") ? "1. Product Name: " : "1. Nume Produs: ");
         cin.ignore();
         getline(cin, numeProdus);
+
+        bool alreadyExists = false;
+        for (const auto &row : csvData)
+        {
+            if (!row.empty() && row[0] == numeProdus)
+            {
+                alreadyExists = true;
+                break;
+            }
+        }
+
+        if (alreadyExists)
+        {
+            cout << ((languageOption == "EN") ? "Product already exists with this name: " : "Produsul cu acest nume exista deja: ") << numeProdus << endl;
+            break;
+        }
+
         cout << ((languageOption == "EN") ? "2. Product Price: " : "2. Pret Produs: ");
         cin >> pretProdus;
-
         // Adaugam produsul la date
         data.push_back({numeProdus, to_string(pretProdus)});
         break;
@@ -101,18 +136,36 @@ void Optiunea6::writeCSVFile(const string &basePath, const string &cafeLocation)
     case 3:
     {
         finalpath = basePath + "/" + cafeLocation + "/" + ((languageOption == "EN") ? "stock.csv" : "stoc.csv");
-        if (newCSV.readCSV(finalpath).empty())
+        auto csvData = newCSV.readCSV(finalpath);
+
+        if (csvData.empty())
         {
             headers = {(languageOption == "EN") ? "Ingredient" : "Ingredient",
                        (languageOption == "EN") ? "Units" : "Unitati"};
         }
 
-        cout << ((languageOption == "EN") ? "1. Ingredient Name (e.g., water) or product (e.g., ground coffee): " : "1. Nume prodBrut(ex:apa)/ingredient(cafeaMacinata): ");
+        cout << ((languageOption == "EN") ? "1. Ingredient Name: " : "1. Nume Ingredient: ");
         cin.ignore();
         getline(cin, ingredientName);
-        cout << ((languageOption == "EN") ? "2. Ingredient/Raw product units (number of units): " : "2. Unitati Ingredient/produsBrut (numarul de unitati): ");
-        cin >> ingredientGramaj;
 
+        bool alreadyExists = false;
+        for (const auto &row : csvData)
+        {
+            if (!row.empty() && row[0] == ingredientName)
+            {
+                alreadyExists = true;
+                break;
+            }
+        }
+
+        if (alreadyExists)
+        {
+            cout << ((languageOption == "EN") ? "Ingredient already exists with this name: " : "Ingredientul cu acest nume exista deja: ") << ingredientName << endl;
+            break;
+        }
+
+        cout << ((languageOption == "EN") ? "2. Ingredient Units: " : "2. Unitati Ingredient: ");
+        cin >> ingredientGramaj;
         // Adaugam ingredientul la date
         data.push_back({ingredientName, to_string(ingredientGramaj)});
         break;
